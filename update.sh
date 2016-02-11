4e8afb2fa55a0b9be1deb0f4914b8c581b5d4c23
Badalyan Vyacheslav <v.badalyan@open-bs.ru>
2016-02-12 01:19:44 +0300

    ReDesign

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+mkdir onbuild-pjproject_trunk-testsute_master
+mkdir onbuild-pjproject_2.4.5-testsute_master
+
+cp -f Dockerfile.template onbuild-pjproject_trunk-testsute_master/Dockerfile
+cp -f Dockerfile.template onbuild-pjproject_2.4.5-testsute_master/Dockerfile
+
+sed -i -e s!pjproject_branch=trunk!pjproject_branch=tags/2.4.5! Dockerfile.template onbuild-pjproject_2.4.5-testsute_master/Dockerfile
+
+
+
