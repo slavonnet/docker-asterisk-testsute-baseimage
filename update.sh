c40aae940297a7e86bc3bcfbace8102fadfaa736
Badalyan Vyacheslav <v.badalyan@open-bs.ru>
2016-02-12 03:51:58 +0300

    Fixes

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ mkdir onbuild-pjproject_2.4.5-testsute_master
 cp -f Dockerfile.template onbuild-pjproject_trunk-testsute_master/Dockerfile
 cp -f Dockerfile.template onbuild-pjproject_2.4.5-testsute_master/Dockerfile
 
-sed -i -e s!pjproject_branch=trunk!pjproject_branch=tags/2.4.5! Dockerfile.template onbuild-pjproject_2.4.5-testsute_master/Dockerfile
+sed -i -e s!pjproject_branch=trunk!pjproject_branch=tags/2.4.5! onbuild-pjproject_2.4.5-testsute_master/Dockerfile
 
 
 
